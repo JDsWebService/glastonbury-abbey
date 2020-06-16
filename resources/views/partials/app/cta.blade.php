dee63eba1b3afdf16bb63cd7051f7f8aa1f222d9
Jonathan Robinson <jdswebservice@gmail.com>
2020-06-16 14:29:03 -0400

    Call To Action | Updated Content

--- resources/views/partials/app/cta.blade.php
+++ resources/views/partials/app/cta.blade.php
@@ -3,9 +3,14 @@
     <div class="container">
 
         <div class="text-center">
-            <h3>COVID-19 Update: Public Worship Suspended</h3>
-            <p>Unfortunately, due to COVID-19, our public worship services are suspended. However, we live stream all our masses on Facebook. Come check us out and dont forget to follow!</p>
-            <p>Mass Times: Monday Thru Saturday @ Noon & Sunday @ 9:30 AM</p>
+            <h3>A Note From Fr. Albrecht Nyce</h3>
+            <p>In order to fight the spread of the COVID-19 virus the Public Mass has closed until further notice. Mass is now streamed <a href="#">Live on Facebook</a>!</p>
+
+            <p>We hope you will understand the measures we are taking in this time of national emergency. By enforcing social distancing measures we can “flatten the curve” and slow down the virus spread.</p>
+
+            <p>Please adhere to all personal safety measures as prescribed by the CDC scientists, and state and local health officials - by doing so you will help not only yourself but others as well, especially the vulnerable.</p>
+
+            <p>Thank you for your patronage and for your understanding. Let us continue to pray for deliverance and wisdom.</p>
             <a class="cta-btn" href="https://www.facebook.com/GlastonburyAbbeyMA/">Check Out Our Facebook</a>
         </div>
     </div>
