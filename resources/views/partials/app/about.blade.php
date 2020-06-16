493101862a0ccf2f8dadcda54c7db1a9d4af1795
Jonathan Robinson <jdswebservice@gmail.com>
2020-06-16 14:52:37 -0400

    About | Updated Content & Added Facebook Information

--- resources/views/partials/app/about.blade.php
+++ resources/views/partials/app/about.blade.php
@@ -7,22 +7,17 @@
                 <img src="img/abbey/covid-19-abbey.jpg" class="img-fluid" alt="">
             </div>
             <div class="col-lg-6 pt-4 pt-lg-0">
-                <h3>COVID-19 Closure</h3>
-                <p>
-                    Glastonbury Abbey remains closed, but the monks are always here for you!
+
+                <img src="/img/facebook.png" class="img-fluid" alt="Check us out on Facebook!">
+                <p class="mt-3">
+                    We are now streaming live to Facebook for all our mass services. Come check us out and don't forget to follow us, that way you're notified every time we go live. Here's Our Daily Schedule!
                 </p>
-                <div class="row">
-                    <div class="col-md-6">
-                        <i class="fas fa-envelope-open-text"></i>
-                        <h4>eMail Contact</h4>
-                        <p>Feel free to reach out to: <a href="mailto:information@glastonburyabbey.org">information@glastonburyabbey.org</a></p>
-                    </div>
-                    <div class="col-md-6">
-                        <i class="fas fa-phone-volume"></i>
-                        <h4>Phone Information</h4>
-                        <p>Give us a call! We'd love to hear from you: <a href="tel:781-749-2155">781-749-2155</a></p>
-                    </div>
-                </div>
+                
+                <h3>Outdoor Mass Is Now Available!</h3>
+                <p>
+                    Glastonbury Abbey is now offering outdoor Sunday Mass! While we still remain vigilant due to COVID-19 we want to extend to you this unique oppurtunity to still be involved. We still will be following the Mass. State's Health Guidelines, and CDC recommendations during this service. If you have any questions feel free to reach out using <a href="#contact" class="scrollto">our contact form</a> below.
+                </p>
+
             </div>
         </div>
 
