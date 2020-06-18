e7e3752db0003cf656582b024606df4abe59499f
Jonathan Robinson <jdswebservice@gmail.com>
2020-06-18 19:48:56 -0400

    About | Updated Facebook Live Image, Changed Descriptions, Added Call To Action Button

--- resources/views/partials/app/about.blade.php
+++ resources/views/partials/app/about.blade.php
@@ -8,14 +8,22 @@
             </div>
             <div class="col-lg-6 pt-4 pt-lg-0">
 
-                <img src="/img/facebook.png" class="img-fluid" alt="Check us out on Facebook!">
-                <p class="mt-3">
-                    We are now streaming live to Facebook for all our mass services. Come check us out and don't forget to follow us, that way you're notified every time we go live.
-                </p>
+                <div class="row justify-content-center">
+                    <div class="col-sm-12 text-center">
+                        <img src="/img/facebooklive.png" id="about-facebook-image" alt="Check us out on Facebook!">
+                    </div>
+                    <div class="col-sm-12 mt-3">
+                        <p class="text-center">
+                            Daily Mass and other prayer services are streamed live on Facebook. Follow us to receive "live" notifications.
+                            <br>
+                            <a href="https://www.facebook.com/GlastonburyAbbeyMA/" class="cta-btn-dark cta-btn-sm">Visit Our Facebook Page</a>
+                        </p>
+                    </div>
+                </div>
                 
-                <h3>Outdoor Mass Is Now Available!</h3>
-                <p>
-                    Glastonbury Abbey is now offering outdoor Sunday Mass! While we still remain vigilant due to COVID-19 we want to extend to you this unique oppurtunity to still be involved. We still will be following the Mass. State's Health Guidelines, and CDC recommendations during this service. If you have any questions feel free to reach out using <a href="#contact" class="scrollto">our contact form</a> below.
+                <h3>Outdoor Mass Is Available!</h3>
+                <p class="text-justify">
+                    As of the First Sunday in July, the Abbey will offer Sunday outdoor Mass at 9:30 AM, weather permitting. While remaining vigilant during COVID-19 we want to extend to you this unique opportunity. The Abbey will be closely following the State guidelines and CDC recommendations. If you have questions please use <a href="#contact" class="scrollto">our contact form</a> below.
                 </p>
 
             </div>
