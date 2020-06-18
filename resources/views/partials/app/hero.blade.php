5d239267e33760a5dfc1d8f7b027828d725a6880
Jonathan Robinson <jdswebservice@gmail.com>
2020-06-18 19:34:51 -0400

    Hero | Updated Title

--- resources/views/partials/app/hero.blade.php
+++ resources/views/partials/app/hero.blade.php
@@ -1,7 +1,7 @@
 <!-- ======= Hero Section ======= -->
 <section id="hero" class="d-flex align-items-center justify-content-center">
 	<div class="container position-relative">
-		<h1>Many New Connection Oppurtunities Are Available</h1>
+		<h1>While The Abbey Church, Retreat House, And Conference Center Remain Closed, New Connections Are Being Made</h1>
 		<h2>We are now offering Mass Livestreamed on Facebook, Outdoor Mass, and much more!</h2>
 		<a href="#about" class="btn-get-started scrollto">Click to Learn More</a>
 	</div>
