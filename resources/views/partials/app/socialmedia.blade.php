186354cdfcc019771fc1c6842e5a29e8ac348aa7
Jonathan Robinson <jdswebservice@gmail.com>
2020-06-18 20:22:10 -0400

    Social Media | Updated Social Media Icons

--- resources/views/partials/app/socialmedia.blade.php
+++ resources/views/partials/app/socialmedia.blade.php
@@ -2,6 +2,5 @@
 	<a href="#" class="twitter"><i class="fab fa-twitter"></i></a>
     <a href="#" class="facebook"><i class="fab fa-facebook-f"></i></a>
     <a href="#" class="instagram"><i class="fab fa-instagram"></i></a>
-    <a href="#" class="google-plus"><i class="fab fa-skype"></i></a>
-    <a href="#" class="linkedin"><i class="fab fa-linkedin-in"></i></a>
+    <a href="#" class="google-plus"><i class="fab fa-youtube"></i></a>
 </div>
